235a5065a6d12e608163c5f86ef140e228897a01
Jason Priebe <priebe@Jasons-MacBook-Pro-3.local>
2019-02-07 22:10:16 -0500

    switched to 8 bar sections; continued improvement of patterns

--- patterns/kick.js
+++ patterns/kick.js
@@ -3,7 +3,22 @@ var HBG = require('../hbgDefs.js')
 module.exports = [
   {
     name: 'four-on-the-floor',
+    length: HBG.M2,
+    notes: [
+      { drum: 'kick', velocity: 90, start: HBG.B111, duration: HBG.D16 },
+      { drum: 'kick', velocity: 90, start: HBG.B121, duration: HBG.D16 },
+      { drum: 'kick', velocity: 90, start: HBG.B131, duration: HBG.D16 },
+      { drum: 'kick', velocity: 90, start: HBG.B141, duration: HBG.D16 },
+      { drum: 'kick', velocity: 90, start: HBG.B211, duration: HBG.D16 },
+      { drum: 'kick', velocity: 90, start: HBG.B221, duration: HBG.D16 },
+      { drum: 'kick', velocity: 90, start: HBG.B231, duration: HBG.D16 },
+      { drum: 'kick', velocity: 90, start: HBG.B241, duration: HBG.D16 }
+    ]
+  },
+  {
+    name: 'four-on-the-floor-w-sumpin-extra',
     length: HBG.M4,
+    weight: 4,
     notes: [
       { drum: 'kick', velocity: 90, start: HBG.B111, duration: HBG.D16 },
       { drum: 'kick', velocity: 90, start: HBG.B121, duration: HBG.D16 },
@@ -20,7 +35,80 @@ module.exports = [
       { drum: 'kick', velocity: 90, start: HBG.B411, duration: HBG.D16 },
       { drum: 'kick', velocity: 90, start: HBG.B421, duration: HBG.D16 },
       { drum: 'kick', velocity: 90, start: HBG.B431, duration: HBG.D16 },
+      { drum: 'kick', velocity: 90, start: HBG.B433, duration: HBG.D16 },
       { drum: 'kick', velocity: 90, start: HBG.B441, duration: HBG.D16 }
+    ]
+  },
+  {
+    name: 'four-on-the-floor-w-sumpin-extra-2',
+    length: HBG.M4,
+    weight: 2,
+    notes: [
+      { drum: 'kick', velocity: 90, start: HBG.B111, duration: HBG.D16 },
+      { drum: 'kick', velocity: 90, start: HBG.B121, duration: HBG.D16 },
+      { drum: 'kick', velocity: 90, start: HBG.B131, duration: HBG.D16 },
+      { drum: 'kick', velocity: 90, start: HBG.B141, duration: HBG.D16 },
+      { drum: 'kick', velocity: 90, start: HBG.B211, duration: HBG.D16 },
+      { drum: 'kick', velocity: 90, start: HBG.B221, duration: HBG.D16 },
+      { drum: 'kick', velocity: 90, start: HBG.B231, duration: HBG.D16 },
+      { drum: 'kick', velocity: 90, start: HBG.B241, duration: HBG.D16 },
+      { drum: 'kick', velocity: 90, start: HBG.B311, duration: HBG.D16 },
+      { drum: 'kick', velocity: 90, start: HBG.B321, duration: HBG.D16 },
+      { drum: 'kick', velocity: 90, start: HBG.B331, duration: HBG.D16 },
+      { drum: 'kick', velocity: 90, start: HBG.B341, duration: HBG.D16 },
+      { drum: 'kick', velocity: 90, start: HBG.B411, duration: HBG.D16 },
+      { drum: 'kick', velocity: 90, start: HBG.B421, duration: HBG.D16 },
+      { drum: 'kick', velocity: 90, start: HBG.B431, duration: HBG.D16 },
+      { drum: 'kick', velocity: 90, start: HBG.B441, duration: HBG.D16 },
+      { drum: 'kick', velocity: 90, start: HBG.B442, duration: HBG.D16 },
+      { drum: 'kick', velocity: 90, start: HBG.B443, duration: HBG.D16 },
+      { drum: 'kick', velocity: 90, start: HBG.B444, duration: HBG.D16 }
+    ]
+  },
+  {
+    name: 'four-on-the-floor-w-sumpin-extra-2',
+    length: HBG.M4,
+    weight: 2,
+    notes: [
+      { drum: 'kick', velocity: 90, start: HBG.B111, duration: HBG.D16 },
+      { drum: 'kick', velocity: 90, start: HBG.B121, duration: HBG.D16 },
+      { drum: 'kick', velocity: 90, start: HBG.B131, duration: HBG.D16 },
+      { drum: 'kick', velocity: 90, start: HBG.B141, duration: HBG.D16 },
+      { drum: 'kick', velocity: 90, start: HBG.B211, duration: HBG.D16 },
+      { drum: 'kick', velocity: 90, start: HBG.B221, duration: HBG.D16 },
+      { drum: 'kick', velocity: 90, start: HBG.B231, duration: HBG.D16 },
+      { drum: 'kick', velocity: 90, start: HBG.B241, duration: HBG.D16 },
+      { drum: 'kick', velocity: 90, start: HBG.B311, duration: HBG.D16 },
+      { drum: 'kick', velocity: 90, start: HBG.B321, duration: HBG.D16 },
+      { drum: 'kick', velocity: 90, start: HBG.B331, duration: HBG.D16 },
+      { drum: 'kick', velocity: 90, start: HBG.B341, duration: HBG.D16 },
+      { drum: 'kick', velocity: 90, start: HBG.B411, duration: HBG.D16 },
+      { drum: 'kick', velocity: 90, start: HBG.B421, duration: HBG.D16 },
+      { drum: 'kick', velocity: 90, start: HBG.B431, duration: HBG.D16 },
+      { drum: 'kick', velocity: 90, start: HBG.B434, duration: HBG.D16 },
+      { drum: 'kick', velocity: 90, start: HBG.B442, duration: HBG.D16 }
+    ]
+  },
+  {
+    name: 'four-on-the-floor-w-sumpin-missin',
+    length: HBG.M4,
+    weight: 1,
+    notes: [
+      { drum: 'kick', velocity: 90, start: HBG.B111, duration: HBG.D16 },
+      { drum: 'kick', velocity: 90, start: HBG.B121, duration: HBG.D16 },
+      { drum: 'kick', velocity: 90, start: HBG.B131, duration: HBG.D16 },
+      { drum: 'kick', velocity: 90, start: HBG.B141, duration: HBG.D16 },
+      { drum: 'kick', velocity: 90, start: HBG.B211, duration: HBG.D16 },
+      { drum: 'kick', velocity: 90, start: HBG.B221, duration: HBG.D16 },
+      { drum: 'kick', velocity: 90, start: HBG.B231, duration: HBG.D16 },
+      { drum: 'kick', velocity: 90, start: HBG.B241, duration: HBG.D16 },
+      { drum: 'kick', velocity: 90, start: HBG.B311, duration: HBG.D16 },
+      { drum: 'kick', velocity: 90, start: HBG.B321, duration: HBG.D16 },
+      { drum: 'kick', velocity: 90, start: HBG.B331, duration: HBG.D16 },
+      { drum: 'kick', velocity: 90, start: HBG.B341, duration: HBG.D16 },
+      { drum: 'kick', velocity: 90, start: HBG.B411, duration: HBG.D16 },
+      { drum: 'kick', velocity: 90, start: HBG.B421, duration: HBG.D16 },
+      { drum: 'kick', velocity: 90, start: HBG.B431, duration: HBG.D16 }
     ]
   }
 ]
--- patterns/perc.js
+++ patterns/perc.js
@@ -10,7 +10,26 @@ module.exports = [
   },
   {
     name: 'perc-1',
+    weight: 3,
     length: HBG.M1,
+    notes: [
+      { drum: 'perc1', velocity: 25, start: HBG.B112, duration: HBG.D16 },
+      { drum: 'perc1', velocity: 25, start: HBG.B132, duration: HBG.D16 }
+    ]
+  },
+  {
+    name: 'perc-1a',
+    weight: 6,
+    length: HBG.M2,
+    notes: [
+      { drum: 'perc1', velocity: 25, start: HBG.B112, duration: HBG.D16 },
+      { drum: 'perc1', velocity: 25, start: HBG.B132, duration: HBG.D16 }
+    ]
+  },
+  {
+    name: 'perc-1b',
+    weight: 6,
+    length: HBG.M4,
     notes: [
       { drum: 'perc1', velocity: 25, start: HBG.B112, duration: HBG.D16 },
       { drum: 'perc1', velocity: 25, start: HBG.B132, duration: HBG.D16 }
@@ -38,6 +57,34 @@ module.exports = [
     ]
   },
   {
+    name: 'perc-3a',
+    length: HBG.M2,
+    notes: [
+      { drum: 'perc1', velocity: 35, start: HBG.B112, duration: HBG.D16 },
+      { drum: 'perc2', velocity: 25, start: HBG.B113, duration: HBG.D16 },
+      { drum: 'perc1', velocity: 35, start: HBG.B132, duration: HBG.D16 },
+      { drum: 'perc2', velocity: 25, start: HBG.B133, duration: HBG.D16 },
+      { drum: 'perc1', velocity: 35, start: HBG.B212, duration: HBG.D16 },
+      { drum: 'perc2', velocity: 25, start: HBG.B213, duration: HBG.D16 }
+    ]
+  },
+  {
+    name: 'perc-3b',
+    length: HBG.M4,
+    notes: [
+      { drum: 'perc1', velocity: 35, start: HBG.B112, duration: HBG.D16 },
+      { drum: 'perc2', velocity: 25, start: HBG.B113, duration: HBG.D16 },
+      { drum: 'perc1', velocity: 35, start: HBG.B132, duration: HBG.D16 },
+      { drum: 'perc2', velocity: 25, start: HBG.B133, duration: HBG.D16 },
+      { drum: 'perc1', velocity: 35, start: HBG.B212, duration: HBG.D16 },
+      { drum: 'perc2', velocity: 25, start: HBG.B213, duration: HBG.D16 },
+      { drum: 'perc1', velocity: 35, start: HBG.B312, duration: HBG.D16 },
+      { drum: 'perc2', velocity: 25, start: HBG.B313, duration: HBG.D16 },
+      { drum: 'perc1', velocity: 35, start: HBG.B332, duration: HBG.D16 },
+      { drum: 'perc2', velocity: 25, start: HBG.B333, duration: HBG.D16 }
+    ]
+  },
+  {
     name: 'perc-4',
     length: HBG.M2,
     notes: [
@@ -47,6 +94,112 @@ module.exports = [
       { drum: 'perc1', velocity: 35, start: HBG.B212, duration: HBG.D16 },
       { drum: 'perc2', velocity: 25, start: HBG.B213, duration: HBG.D16 },
       { drum: 'perc2', velocity: 25, start: HBG.B233, duration: HBG.D16 }
+    ]
+  },
+  {
+    name: 'perc-4a',
+    length: HBG.M4,
+    notes: [
+      { drum: 'perc1', velocity: 35, start: HBG.B112, duration: HBG.D16 },
+      { drum: 'perc2', velocity: 25, start: HBG.B113, duration: HBG.D16 },
+      { drum: 'perc2', velocity: 25, start: HBG.B133, duration: HBG.D16 },
+      { drum: 'perc1', velocity: 35, start: HBG.B212, duration: HBG.D16 },
+      { drum: 'perc2', velocity: 25, start: HBG.B213, duration: HBG.D16 },
+      { drum: 'perc2', velocity: 25, start: HBG.B233, duration: HBG.D16 }
+    ]
+  },
+  {
+    name: 'perc-5',
+    length: HBG.M2,
+    notes: [
+      { drum: 'perc2', velocity: 45, start: HBG.B111, duration: HBG.D16 },
+      { drum: 'perc2', velocity: 45, start: HBG.B113, duration: HBG.D16 },
+      { drum: 'perc2', velocity: 45, start: HBG.B121, duration: HBG.D16 },
+      { drum: 'perc2', velocity: 45, start: HBG.B122, duration: HBG.D16 },
+      { drum: 'perc2', velocity: 45, start: HBG.B221, duration: HBG.D16 },
+      { drum: 'perc2', velocity: 45, start: HBG.B222, duration: HBG.D16 }
+    ]
+  },
+  {
+    name: 'perc-5a',
+    length: HBG.M4,
+    notes: [
+      { drum: 'perc2', velocity: 45, start: HBG.B111, duration: HBG.D16 },
+      { drum: 'perc2', velocity: 45, start: HBG.B113, duration: HBG.D16 },
+      { drum: 'perc2', velocity: 45, start: HBG.B121, duration: HBG.D16 },
+      { drum: 'perc2', velocity: 45, start: HBG.B122, duration: HBG.D16 },
+      { drum: 'perc2', velocity: 45, start: HBG.B221, duration: HBG.D16 },
+      { drum: 'perc2', velocity: 45, start: HBG.B222, duration: HBG.D16 }
+    ]
+  },
+  {
+    name: 'perc-6',
+    length: HBG.M2,
+    notes: [
+      { drum: 'perc2', velocity: 45, start: HBG.B111, duration: HBG.D16 },
+      { drum: 'perc2', velocity: 45, start: HBG.B112, duration: HBG.D16 },
+      { drum: 'perc2', velocity: 45, start: HBG.B114, duration: HBG.D16 },
+      { drum: 'perc2', velocity: 45, start: HBG.B122, duration: HBG.D16 },
+      { drum: 'perc2', velocity: 45, start: HBG.B221, duration: HBG.D16 },
+      { drum: 'perc2', velocity: 45, start: HBG.B222, duration: HBG.D16 }
+    ]
+  },
+  {
+    name: 'perc-6a',
+    length: HBG.M4,
+    notes: [
+      { drum: 'perc2', velocity: 45, start: HBG.B111, duration: HBG.D16 },
+      { drum: 'perc2', velocity: 45, start: HBG.B112, duration: HBG.D16 },
+      { drum: 'perc2', velocity: 45, start: HBG.B114, duration: HBG.D16 },
+      { drum: 'perc2', velocity: 45, start: HBG.B122, duration: HBG.D16 },
+      { drum: 'perc2', velocity: 45, start: HBG.B221, duration: HBG.D16 },
+      { drum: 'perc2', velocity: 45, start: HBG.B222, duration: HBG.D16 }
+    ]
+  },
+  {
+    name: 'perc-7',
+    length: HBG.M2,
+    notes: [
+      { drum: 'perc2', velocity: 45, start: HBG.B111, duration: HBG.D16 },
+      { drum: 'perc2', velocity: 45, start: HBG.B112, duration: HBG.D16 },
+      { drum: 'perc2', velocity: 45, start: HBG.B144, duration: HBG.D16 },
+      { drum: 'perc2', velocity: 45, start: HBG.B142, duration: HBG.D16 },
+      { drum: 'perc2', velocity: 45, start: HBG.B222, duration: HBG.D16 }
+    ]
+  },
+  {
+    name: 'perc-7a',
+    length: HBG.M4,
+    notes: [
+      { drum: 'perc2', velocity: 45, start: HBG.B111, duration: HBG.D16 },
+      { drum: 'perc2', velocity: 45, start: HBG.B112, duration: HBG.D16 },
+      { drum: 'perc2', velocity: 45, start: HBG.B144, duration: HBG.D16 },
+      { drum: 'perc2', velocity: 45, start: HBG.B142, duration: HBG.D16 },
+      { drum: 'perc2', velocity: 45, start: HBG.B222, duration: HBG.D16 }
+    ]
+  },
+  {
+    name: 'perc-8',
+    length: HBG.M2,
+    notes: [
+      { drum: 'perc1', velocity: 45, start: HBG.B111, duration: HBG.D16 },
+      { drum: 'perc1', velocity: 45, start: HBG.B112, duration: HBG.D16 },
+      { drum: 'perc2', velocity: 45, start: HBG.B124, duration: HBG.D16 },
+      { drum: 'perc2', velocity: 45, start: HBG.B133, duration: HBG.D16 },
+      { drum: 'perc1', velocity: 45, start: HBG.B211, duration: HBG.D16 },
+      { drum: 'perc1', velocity: 45, start: HBG.B214, duration: HBG.D16 }
+    ]
+  },
+  {
+    name: 'perc-8a',
+    length: HBG.M4,
+    notes: [
+      { drum: 'perc1', velocity: 45, start: HBG.B111, duration: HBG.D16 },
+      { drum: 'perc1', velocity: 45, start: HBG.B112, duration: HBG.D16 },
+      { drum: 'perc2', velocity: 45, start: HBG.B124, duration: HBG.D16 },
+      { drum: 'perc2', velocity: 45, start: HBG.B133, duration: HBG.D16 },
+      { drum: 'perc1', velocity: 45, start: HBG.B211, duration: HBG.D16 },
+      { drum: 'perc1', velocity: 45, start: HBG.B214, duration: HBG.D16 }
     ]
   }
 ]
