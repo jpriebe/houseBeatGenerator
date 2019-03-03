2b518dcbb2feb87121e2aa89053f78e029736b2a
Jason Priebe <jason.priebe@gmail.com>
2019-03-03 15:02:08 -0500

    added some more kick patterns

--- patterns/kick.js
+++ patterns/kick.js
@@ -241,5 +241,117 @@ module.exports = [
       { drum: 'kick', velocity: 90, start: HBG.B431, duration: HBG.D16 },
       { drum: 'kick', velocity: 90, start: HBG.B441, duration: HBG.D16 }
     ]
+  },
+  {
+    name: 'k-10a',
+    length: HBG.M2,
+    weight: 1,
+    notes: [
+      { drum: 'kick', velocity: 90, start: HBG.B111, duration: HBG.D16 },
+      { drum: 'kick', velocity: 90, start: HBG.B121, duration: HBG.D16 },
+      { drum: 'kick', velocity: 90, start: HBG.B131, duration: HBG.D16 },
+      { drum: 'kick', velocity: 90, start: HBG.B141, duration: HBG.D16 },
+      { drum: 'kick', velocity: 90, start: HBG.B211, duration: HBG.D16 },
+      { drum: 'kick', velocity: 90, start: HBG.B214, duration: HBG.D16 },
+      { drum: 'kick', velocity: 90, start: HBG.B222, duration: HBG.D16 },
+      { drum: 'kick', velocity: 90, start: HBG.B231, duration: HBG.D16 },
+      { drum: 'kick', velocity: 90, start: HBG.B241, duration: HBG.D16 }
+    ]
+  },
+  {
+    name: 'k-10b',
+    length: HBG.M2,
+    weight: 1,
+    notes: [
+      { drum: 'kick', velocity: 90, start: HBG.B111, duration: HBG.D16 },
+      { drum: 'kick', velocity: 90, start: HBG.B114, duration: HBG.D16 },
+      { drum: 'kick', velocity: 90, start: HBG.B122, duration: HBG.D16 },
+      { drum: 'kick', velocity: 90, start: HBG.B131, duration: HBG.D16 },
+      { drum: 'kick', velocity: 90, start: HBG.B241, duration: HBG.D16 },
+      { drum: 'kick', velocity: 90, start: HBG.B211, duration: HBG.D16 },
+      { drum: 'kick', velocity: 90, start: HBG.B221, duration: HBG.D16 },
+      { drum: 'kick', velocity: 90, start: HBG.B231, duration: HBG.D16 },
+      { drum: 'kick', velocity: 90, start: HBG.B241, duration: HBG.D16 }
+    ]
+  },
+  {
+    name: 'k-10c',
+    length: HBG.M4,
+    weight: 1,
+    notes: [
+      { drum: 'kick', velocity: 90, start: HBG.B111, duration: HBG.D16 },
+      { drum: 'kick', velocity: 90, start: HBG.B121, duration: HBG.D16 },
+      { drum: 'kick', velocity: 90, start: HBG.B131, duration: HBG.D16 },
+      { drum: 'kick', velocity: 90, start: HBG.B141, duration: HBG.D16 },
+      { drum: 'kick', velocity: 90, start: HBG.B211, duration: HBG.D16 },
+      { drum: 'kick', velocity: 90, start: HBG.B221, duration: HBG.D16 },
+      { drum: 'kick', velocity: 90, start: HBG.B231, duration: HBG.D16 },
+      { drum: 'kick', velocity: 90, start: HBG.B241, duration: HBG.D16 },
+      { drum: 'kick', velocity: 90, start: HBG.B311, duration: HBG.D16 },
+      { drum: 'kick', velocity: 90, start: HBG.B321, duration: HBG.D16 },
+      { drum: 'kick', velocity: 90, start: HBG.B331, duration: HBG.D16 },
+      { drum: 'kick', velocity: 90, start: HBG.B341, duration: HBG.D16 },
+      { drum: 'kick', velocity: 90, start: HBG.B411, duration: HBG.D16 },
+      { drum: 'kick', velocity: 90, start: HBG.B414, duration: HBG.D16 },
+      { drum: 'kick', velocity: 90, start: HBG.B422, duration: HBG.D16 },
+      { drum: 'kick', velocity: 90, start: HBG.B431, duration: HBG.D16 },
+      { drum: 'kick', velocity: 90, start: HBG.B441, duration: HBG.D16 }
+    ]
+  },
+  {
+    name: 'k-11a',
+    length: HBG.M2,
+    weight: 1,
+    notes: [
+      { drum: 'kick', velocity: 90, start: HBG.B111, duration: HBG.D16 },
+      { drum: 'kick', velocity: 90, start: HBG.B121, duration: HBG.D16 },
+      { drum: 'kick', velocity: 90, start: HBG.B131, duration: HBG.D16 },
+      { drum: 'kick', velocity: 90, start: HBG.B141, duration: HBG.D16 },
+      { drum: 'kick', velocity: 90, start: HBG.B211, duration: HBG.D16 },
+      { drum: 'kick', velocity: 90, start: HBG.B214, duration: HBG.D16 },
+      { drum: 'kick', velocity: 90, start: HBG.B223, duration: HBG.D16 },
+      { drum: 'kick', velocity: 90, start: HBG.B231, duration: HBG.D16 },
+      { drum: 'kick', velocity: 90, start: HBG.B241, duration: HBG.D16 }
+    ]
+  },
+  {
+    name: 'k-11b',
+    length: HBG.M2,
+    weight: 1,
+    notes: [
+      { drum: 'kick', velocity: 90, start: HBG.B241, duration: HBG.D16 },
+      { drum: 'kick', velocity: 90, start: HBG.B111, duration: HBG.D16 },
+      { drum: 'kick', velocity: 90, start: HBG.B114, duration: HBG.D16 },
+      { drum: 'kick', velocity: 90, start: HBG.B123, duration: HBG.D16 },
+      { drum: 'kick', velocity: 90, start: HBG.B131, duration: HBG.D16 },
+      { drum: 'kick', velocity: 90, start: HBG.B141, duration: HBG.D16 },
+      { drum: 'kick', velocity: 90, start: HBG.B211, duration: HBG.D16 },
+      { drum: 'kick', velocity: 90, start: HBG.B221, duration: HBG.D16 },
+      { drum: 'kick', velocity: 90, start: HBG.B231, duration: HBG.D16 }
+    ]
+  },
+  {
+    name: 'k-11c',
+    length: HBG.M4,
+    weight: 1,
+    notes: [
+      { drum: 'kick', velocity: 90, start: HBG.B111, duration: HBG.D16 },
+      { drum: 'kick', velocity: 90, start: HBG.B121, duration: HBG.D16 },
+      { drum: 'kick', velocity: 90, start: HBG.B131, duration: HBG.D16 },
+      { drum: 'kick', velocity: 90, start: HBG.B141, duration: HBG.D16 },
+      { drum: 'kick', velocity: 90, start: HBG.B211, duration: HBG.D16 },
+      { drum: 'kick', velocity: 90, start: HBG.B221, duration: HBG.D16 },
+      { drum: 'kick', velocity: 90, start: HBG.B231, duration: HBG.D16 },
+      { drum: 'kick', velocity: 90, start: HBG.B241, duration: HBG.D16 },
+      { drum: 'kick', velocity: 90, start: HBG.B311, duration: HBG.D16 },
+      { drum: 'kick', velocity: 90, start: HBG.B321, duration: HBG.D16 },
+      { drum: 'kick', velocity: 90, start: HBG.B331, duration: HBG.D16 },
+      { drum: 'kick', velocity: 90, start: HBG.B341, duration: HBG.D16 },
+      { drum: 'kick', velocity: 90, start: HBG.B411, duration: HBG.D16 },
+      { drum: 'kick', velocity: 90, start: HBG.B414, duration: HBG.D16 },
+      { drum: 'kick', velocity: 90, start: HBG.B423, duration: HBG.D16 },
+      { drum: 'kick', velocity: 90, start: HBG.B431, duration: HBG.D16 },
+      { drum: 'kick', velocity: 90, start: HBG.B441, duration: HBG.D16 }
+    ]
   }
 ]
